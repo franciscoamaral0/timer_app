31a9322ad94540265018d877d26416c4997211d5
franciscoamaral <francisco.amaral@arquiconsult.com>
2022-08-19 21:33:14 +0100

    home partial done

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,35 @@
+import { Play } from 'phosphor-react'
+import { CountDown, FormContainer, HomeContainer, Separator } from './styles'
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form>
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input id="task" type="text" />
+
+          <label htmlFor="minutesAmount"></label>
+          <input type="number" id="minutesAmount" />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountDown>
+          <span>0</span>
+          <span>0</span>
+
+          <Separator>:</Separator>
+
+          <span>0</span>
+          <span>0</span>
+        </CountDown>
+
+        <button type="submit">
+          <Play size={24} />
+          Começar
+        </button>
+      </form>
+    </HomeContainer>
+  )
+}
