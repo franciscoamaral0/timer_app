91a619dc3a88123168c8ff156deda6cc9ab0df96
franciscoamaral <francisco.amaral@arquiconsult.com>
2022-08-19 21:13:04 +0100

    created layout, header layout and implements react routes

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import { HeaderContainer } from './styles'
+import Logo from '../../assets/Logo.svg'
+import { Scroll, Timer } from 'phosphor-react'
+import { NavLink } from 'react-router-dom'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <img src={Logo} alt="" />
+      <nav>
+        <NavLink to={'/'} title="timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to={'/history'} title="histórico">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  )
+}
